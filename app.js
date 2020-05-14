9b715d2d74f1efdf713ae576920551f1036b9b7b
fbry-alx <fabry.pappacena@gmail.com>
2020-05-14 14:37:33 -0700

    created array of words

---
+++ app.js
@@ -0,0 +1,51 @@
+const word = document.querySelector('.word');
+const text = document.getElementById('text');
+const scoreEl = document.getElementById('score');
+const timeEl = document.getElementById('time');
+const endgameEl = document.querySelector('.end-game');
+const headingTwo = document.querySelector('h2');
+const colorSwitch = document.querySelector('.switch-color-btn');
+const rulesBtn = document.querySelector('.rules-btn');
+const rulesDiv = document.getElementById('rules');
+const closeBtn = document.getElementById('close');
+
+
+/* words array */
+const words = [
+  'tacky',
+  'hungry',
+  'drive',
+  'recruit',
+  'scene',
+  'organize',
+  'massive',
+  'skate',
+  'receipt',
+  'brick',
+  'paint',
+  'light',
+  'waves',
+  'silver',
+  'photo',
+  'ignore',
+  'announce',
+  'modern',
+  'purpose',
+  'immerse',
+  'observe',
+  'novel',
+  'sharp',
+  'shatter',
+  'fragile',
+  'strike',
+  'island',
+  'rainy',
+  'permit',
+  'convert',
+  'brash',
+  'thought',
+  'separate',
+  'game',
+  'smiling',
+  'meeting'
+];
